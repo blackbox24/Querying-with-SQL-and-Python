d417ddbab89534a38ddf7c10f79e15ba63a91965
blackbox24 <nicardid@gmail.com>
2026-04-09 10:01:32 +0000

    chore: answered Which are the 10 best-selling tracks

---
+++ README.md
@@ -0,0 +1,13 @@
+# Querying with SQL and Python
+
+## Write SQL queries to answer the following questions
+
+- Which are the 10 best-selling tracks?
+
+- Which country generates the most revenue?
+
+- Who is the top-performing sales employee?
+
+- Load query results into Pandas DataFrames
+
+- Visualize at least one result with a bar chart
--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 import sqlite3
+from decouple import config
 
-conn = sqlite3.connect("")
+DATABASE_URL = config("DATABASE_URL",cast=str,default="./db.sqlite")
+conn = sqlite3.connect(DATABASE_URL) # type: ignore
+
+cur = conn.cursor()
+
+
+track_info = cur.execute("PRAGMA table_info(InvoiceLine)");
+print("#" * 20)
+print(*track_info, sep="\n")
+print("#" * 20)
+
+# Top 10 best-selling tracks
+best_selling_tracks = cur.execute("" \
+"""
+SELECT 
+    COUNT(tr.Name) AS n_distinct_names, 
+    lnl.Quantity,
+    tr.Name,
+    ROUND(tr.UnitPrice * COUNT(tr.Name), 2) AS total_price
+FROM Track tr
+INNER JOIN InvoiceLine lnl ON tr.TrackId = lnl.TrackId
+GROUP BY tr.Name
+ORDER BY n_distinct_names DESC
+LIMIT 10
+""")
+print(*best_selling_tracks, sep="\n")
+cur.close()
